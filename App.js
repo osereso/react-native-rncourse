f1c29845e2b8a92ac340a25e9dd7c92b1c0464e2
mbp <mbp@MBP-15-srs.local>
2019-02-04 11:33:34 +0100

    Flatlist and delete item

--- App.js
+++ App.js
@@ -1,48 +1,42 @@
-import React, { Component } from 'react';
-import {Platform, StyleSheet, Text, View, TextInput, Button} from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
 
-import ListItem from './src/components/ListItem/ListItem';
 import PlaceInput from "./src/components/PlaceInput/PlaceInput";
 import PlaceList from "./src/components/PlaceList/PlaceList";
 
 export default class App extends Component {
-
   state = {
-    placeName: '',
     places: []
-  }
+  };
 
   placeAddedHandler = placeName => {
     this.setState(prevState => {
       return {
-        places: prevState.places.concat(placeName)
+        places: prevState.places.concat({
+          key: Math.random(), 
+          value: placeName
+        })
       };
     });
   };
 
-
-  placeNameChangeHandler = val => {
-    this.setState({
-      placeName: val
-    });
-  }
-
-  placeSubmitHandler = () => {
-    if (this.state.placeName.trim() === "") {
-      return;
-    }
+  placeDeletedHandler = key => {
     this.setState(prevState => {
       return {
-        places: prevState.places.concat(prevState.placeName)
+        places: prevState.places.filter(place => {
+          return place.key !== key;
+        })
       };
-    } )
+    });
   }
 
   render() {
     return (
       <View style={styles.container}>
         <PlaceInput onPlaceAdded={this.placeAddedHandler} />
-        <PlaceList places={this.state.places} />
+        <PlaceList 
+          places={this.state.places} 
+          onItemDeleted={this.placeDeletedHandler} />
       </View>
     );
   }
